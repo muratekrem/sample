d617d73e08a3ce32910764907e5c8fc6ab8e8efe
muratekrem <110377394+muratekrem@users.noreply.github.com>
2024-11-18 03:45:09 +0300

    db.sql

---
+++ db/db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE react_auth;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+SELECT * FROM react_auth.users;
+
